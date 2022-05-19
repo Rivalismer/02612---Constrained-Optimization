fa49667278a4e0bc0e1f44a73bf254838b8a3d4a
GregFrog <102669319+grzegorzzaba@users.noreply.github.com>
2022-05-20 01:33:23 +0200

    Add files via upload

--- Exam/Problem2/Question_6.m
+++ Exam/Problem2/Question_6.m
@@ -1,9 +1,18 @@
-% clc, clear, close all
+clc, clear, close all
+%{
+    This program is investigating set quadratic problem, while changing one
+    value in  b  matrix of equality constraints. It is using primal-dual
+    interiot point, quadprog and cvx gurobi optimization algorithms.
 
+    For this program to work, cvx libary needs to be configured for matlab
+%}
 % Setting quadprog not to show notifications
 options_qp = optimoptions('quadprog', 'Display', 'off');
+% Setting cvx solver for Gurobi
+cvx_solver Gurobi
+methods = {'PDIP', 'quadprog', 'Gurobi'}
 
-%% Input data - H, g, A, b, l, u matrices
+%% Input data: H, g, A, b, l, u matrices
 
 H = [5.0000 1.8600 1.2400 1.4800 -0.4600
     1.8600 3.0000 0.4400 1.1200 0.5200
@@ -29,6 +38,9 @@ b = [15
 l = zeros(5, 1);
 u = ones(5, 1);
 
+% Vector of investigated b values
+bs = [8.5 : 0.2 : 18.68, 18.68];
+
 %% Initial guesses for estimated variables
 x0 = [0.5; 0.5; 0.5; 0.5; 0.5];
 y0 = [1; 1];
@@ -37,20 +49,94 @@ s0 = [0.5; 0.5; 0.5; 0.5; 0.5; 0.5; 0.5; 0.5; 0.5; 0.5];
 
 %% Calculating optimal value with use of different methods
 
-% Primal dual interior point implemented by us
-disp('Primal dual interior point')
-tic
-[x_primaldual, iter, converged] = PrimalDualInteriorPoint(x0, y0, s0, z0, H, g, A, b, l, u);
-toc
+% Loop for algorithms performance investigation on considered problem, the
+% loop will continue to run even though algorithm would show error
+for i = 1:1:length(bs)
+    b(1) = bs(i);
+    % Primal dual interior point algorithm
+    try    
+        tic
+        [x_pdip, iter_pdip] = PrimalDualInteriorPoint(x0, y0, s0, z0, H, g, A, b, l, u);
+        cputime_pdip = toc;
+    catch
+        fprintf('Primal-dual interior point\nError occured for b = %f', bs(i))
+        x_pdip = zeros(5,1);
+        iter_pdip = 0; 
+        cputime_pdip = 0;
+    end
+    % Quadprog
+    try
+        tic
+        [x_qp, ~, ~, stats_qp] = quadprog(H, g, [], [], A', b, l, u, [], options_qp);
+        cputime_qp = toc;
+    catch 
+        fprintf('Quadprog\nError occured for b = %f', bs(i))
+        x_qp = zeros(5,1);
+        stats_qp.iterations = 0; 
+        cputime_qp = 0;
+    end
+    % CVX Gurobi
+    try
+        tic
+        cvx_begin
+            variable x_gb(5)
+            minimize(1/2*x_gb'*H*x_gb + g'*x_gb)
+            subject to
+                A' * x_gb == b
+                x_gb <= u
+                x_gb >= l
+        cvx_end 
+        cputime_gb = toc;
+    catch
+        printf('Gurobi\nError occured for b = %f', bs(i))
+        x_gb = zeros(5,1);
+        iter_gb = 0; 
+        cputime_gb = 0;
+    end
+    % Saving results for all algorithms
+    stats.x_pdip(:, i) = x_pdip;    % primal-dual minimizer
+    stats.x_qp(:, i) = x_qp;        % quadprog minimizer
+    stats.x_gb(:, i) = x_gb;        % gurobi minimizer
+    stats.iter(i, :) = [iter_pdip, stats_qp.iterations];  % iterations number 
+    stats.cpu(i, :) = [cputime_pdip, cputime_qp, cputime_gb]; % cpu time
+    stats.opt_val(i) = cvx_optval;  % optimum value
+end
 
-% MATLAB quadoprog function without initial guess
-disp('quadprog')
-tic
-[x_quadprog, ~, ~, stats] = quadprog(H, g, [], [], A', b, l, u, [], options_qp);
-toc
+%% Plotting results
 
-% MATLAB quadoprog function with same initial guess as interior point
-disp('quadprog')
-tic
-[x_quadprog, ~, ~, stats] = quadprog(H, g, [], [], A', b, l, u, x0, options_qp);
-toc
+figure(1);
+scatter(bs, stats.opt_val, 30, 'filled');
+title('Optimal objective function value');
+xlabel('b(1) value');
+ylabel('Optimal value');
+set(gca, "FontSize", 12);
+
+figure(2);
+scatter(bs, vecnorm(stats.x_gb), 30, 'filled');
+title('L-2 norm of reached minimizer');
+xlabel('b(1) value');
+ylabel('x_o_p_t norm');
+set(gca, "FontSize", 12);
+
+figure(3);
+hold on;
+scatter(bs, stats.cpu(:, 1), 30, 'r', 'filled');
+scatter(bs, stats.cpu(:, 2), 30, 'm', 'filled');
+scatter(bs, stats.cpu(:, 3), 30, 'g', 'filled');
+title('Computation time for all methods');
+xlabel('b(1) value');
+ylabel('Time (s)');
+set(gca, "FontSize", 12);
+legend({'primal-dual','quadprog', 'gurobi'}, 'Location', 'best');
+hold off;
+
+figure(4);
+hold on;
+scatter(bs, stats.iter(:, 1), 30, 'filled');
+scatter(bs, stats.iter(:, 2), 30, 'filled');
+title('Number of iterations for selected methods');
+xlabel('b(1) value');
+ylabel('Iterations number');
+set(gca, "FontSize", 12);
+legend({'primal-dual','quadprog'}, 'Location', 'best')
+hold off;
